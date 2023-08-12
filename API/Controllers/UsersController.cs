4af2c8535add7feff702748fa7916230d936a728
Ehsan ullah kayani <e_kayani@hotmail.com>
2023-08-12 23:00:19 +0500

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]  // .com/api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DbManager _context;
+
+        public UsersController(DbManager context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> getallUsers()
+        {
+            var allUsers = await _context.Users.ToListAsync();
+
+            return allUsers;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> getUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user;
+        }
+    }
+}
---
+++ API/Data/DbManager.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DbManager : DbContext
+    {
+        public DbManager(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
